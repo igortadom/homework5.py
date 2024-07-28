f4779235cd12ebf292d4498cd43cf47efc56f349
igortadom <tadom@yandex.ru>
2024-07-28 06:00:29 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+tuple_ =( 1, True, 'apple', 'd',['a', 8])
+immutable_var = tuple_
+print(immutable_var)
+immutable_var[4][1] = 5
+print(immutable_var)
+mutable_list = ['face', 'nose','eye', 5]
+mutable_list.append(False)
+print(mutable_list)
+mutable_list.extend('error')
+print(mutable_list)
+immutable_var[2]['peach']
+#print(immutable_var)
+
+
